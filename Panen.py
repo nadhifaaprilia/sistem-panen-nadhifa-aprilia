4de12eaa1c46e0d86cccc87d1e43d95b448f1eb2
Nadhifa Aprilia <ndhfaaprilia@apps.ipb.ac.id>
2026-09-17 16:07:27 +0700

    Menambahkan program penghitung hasil panen

---
+++ Panen.py
@@ -0,0 +1,9 @@
+nama_komoditas = "Cabai"
+jumlah_panen = 100
+harga_per_kg = 15000
+
+total = jumlah_panen * harga_per_kg
+
+print("Komoditas:", nama_komoditas)
+print("Jumlah panen:", jumlah_panen, "kg")
+print("Total nilai panen: Rp", total)
